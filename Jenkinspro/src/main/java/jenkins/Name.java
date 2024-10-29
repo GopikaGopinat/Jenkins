39f241d903b6c3ee4edad73abce7c506072194c1
gopik <gopik@Gopika>
2024-10-29 20:35:48 +0530

    finished project

---
+++ Jenkinspro/src/main/java/jenkins/Name.java
@@ -0,0 +1,15 @@
+package jenkins;
+
+public class Name {
+	String s;
+	public void name(String s)
+	{
+		System.out.println("Name is "+s);
+	}
+	public static void main(String[] args) {
+		Name ob= new Name();
+		ob.name("Gopika");
+		
+	}
+
+}
